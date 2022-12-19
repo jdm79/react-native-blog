c9929579387e2602983ef43405ce6829cb7b2a27
jdm79 <james.malvern@gmail.com>
2022-12-19 22:19:59 +0000

    First commit: added BlogProvider to use Context to share state

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import IndexScreen from './src/screens/indexScreen';
+import { BlogProvider } from './src/context/BlogContext';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const navigator = createStackNavigator({
+  Index: IndexScreen
+}, {
+  initialRouteName: 'Index',
+  defaultNavigationOptions: {
+    title: 'Blogs'
+  }
 });
+
+const App = createAppContainer(navigator);
+
+// below we have wrapped App component as a child of the BlogProvider parent component
+// like this we can pass state down from absolute top
+export default () => {
+  return <BlogProvider>
+      <App />
+    </BlogProvider>;
+}
