dedb5b3e95438bb590d841f9f91354c1542d4791
jdm79 <james.malvern@gmail.com>
2022-12-21 18:50:00 +0000

    Make the array dynamic by using useState hook and making addBlogPost function available to other components

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,21 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
-    const blogPosts = [
-        { title: "Making web scrapers"},
-        { title: "Using the terminal to web development"},
-        { title: "Basic chrome extensions"},
-        { title: "Getting started with Mastodon"},
-        { title: "Building a Twitter bot (awks)"},
-        { title: "Building a web-scraping bot"},
-        { title: "Making a web-scraper and sending data to a database"},
-        { title: "Running Python scripts on your terminal"},
-        { title: "Using GitHub for dev job interviews"}
-    ]
+    const [blogPosts, setBlogPosts] = useState([]);
 
-    return <BlogContext.Provider value={blogPosts}>
+    const addBlogPost = () => {
+        setBlogPosts([...blogPosts, {title: `Blog Post #${blogPosts.length + 1}`}])
+    }
+
+    return <BlogContext.Provider value={{data: blogPosts, addBlogPost}}>
         {children}
     </BlogContext.Provider>;
 };
