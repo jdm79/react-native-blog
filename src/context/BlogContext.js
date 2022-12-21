9c291ad42b8f0ba307b398f1a1e1d7a7e5667f0a
jdm79 <james.malvern@gmail.com>
2022-12-21 19:39:02 +0000

    Remove delete posts, replace useState with useReducer and add switch statement for different actions, so everything is a bit neater further down the road

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,19 +1,24 @@
-import React, { useState } from "react";
-
+import React, { useReducer } from "react";
+// Context only communicates state. state is still handled by useState hook. like props.
 const BlogContext = React.createContext();
 
+const blogReducer = (state, action) => {
+    switch (action.type) {
+        case 'add_blogpost':
+            return [...state, { title: `Blog post #${state.length + 1}`}];
+        default:
+            return state;
+    }
+}
+
 export const BlogProvider = ({ children }) => {
-    const [blogPosts, setBlogPosts] = useState([]);
+    const [blogPosts, dispatch] = useReducer(blogReducer, []);
 
     const addBlogPost = () => {
-        setBlogPosts([...blogPosts, {title: `Blog Post #${blogPosts.length + 1}`}])
-    }
+        dispatch({ type: 'add_blogpost'})
+    };
 
-    const deleteBlogPosts = () => {
-        setBlogPosts([])
-    }
-
-    return <BlogContext.Provider value={{data: blogPosts, addBlogPost, deleteBlogPosts}}>
+    return <BlogContext.Provider value={{data: blogPosts, addBlogPost}}>
         {children}
     </BlogContext.Provider>;
 };
--- src/screens/indexScreen.js
+++ src/screens/indexScreen.js
@@ -3,13 +3,12 @@ import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
 import BlogContext from "../context/BlogContext";
 
 const IndexScreen = () => {
-    const { data, addBlogPost, deleteBlogPosts} = useContext(BlogContext);
+    const { data, addBlogPost} = useContext(BlogContext);
 
     return (
         <View>
             <Text>2023 Blog Posts</Text>
             <Button onPress={addBlogPost} title={"Add blog post"}/>
-            <Button onPress={deleteBlogPosts} title={"Delete blog posts"}/>
             <FlatList 
                 data={data}
                 keyExtractor={(blogPost) => blogPost.title}
